a26c52e211c8b0ef75deaabe06d4b9b77719ea33
ccie20079 <ccie20079@126.com>
2020-01-17 18:09:27 +0800

    test
    2020 01 17 test

--- src/com/dukepu/utils/DBHelper.java
+++ src/com/dukepu/utils/DBHelper.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBHelper {
 			//	 public static final String url = "jdbc:mysql://127.0.0.1/test";  
-		 public static final String url = "dbc:oracle:thin:@192.168.10.243:1521:dukepu";  
+		 public static final String url = "dbc:oracle:thin:@192.168.8.40:1521:dukepu";  
 		 //public static final String name = "com.mysql.jdbc.Driver";  
 		 public static final String name = "oracle.jdbc.driver.OracleDriver";  
 		    public static final String user = "dukepu";  
